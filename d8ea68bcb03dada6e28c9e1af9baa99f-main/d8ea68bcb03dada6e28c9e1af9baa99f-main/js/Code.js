825cd1ba05374a1d712aedd94aeae021b9a4cfd9
Amruta098 <83020702+Amruta098@users.noreply.github.com>
2021-07-29 19:56:03 +0530

    Add files via upload

---
+++ d8ea68bcb03dada6e28c9e1af9baa99f-main/d8ea68bcb03dada6e28c9e1af9baa99f-main/js/Code.js
@@ -0,0 +1,34 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+ 
+    fill("white")
+    textSize(15)
+    text("N T O N A F I C", 700,150)
+    fill("lightblue")
+    text("Hint: PERFORL A TASK !!", 700,170)
+ 
+    fill("white")
+    textSize(15)
+    text("T A B A S E D A", 100,250)
+    fill("lightblue")
+    text("Hint: STORES INFORMATION !!", 100,270)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
